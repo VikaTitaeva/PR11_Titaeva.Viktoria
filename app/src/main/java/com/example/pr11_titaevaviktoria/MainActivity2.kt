ceb99b93d3f95bf14c9189ed5b580c56784de373
VikaTitaeva <vika.titaeva.2004@mail.ru>
2022-06-17 21:15:05 +0700

    Все сделано

---
+++ app/src/main/java/com/example/pr11_titaevaviktoria/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.pr11_titaevaviktoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pr11_titaevaviktoria.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
